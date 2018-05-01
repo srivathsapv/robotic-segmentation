241763c1d06f4eafe13daff81f30055cc4c86845
vishalrao <vishal.srini.rao@gmail.com>
2018-05-01 09:02:07 -0400

    additional mask rcnn modifications

--- pytorch_mask_rcnn_2/coco.py
+++ pytorch_mask_rcnn_2/coco.py
@@ -516,7 +516,7 @@ if __name__ == '__main__':
         print("Training network heads")
         model.train_model(dataset_train, dataset_val,
                     learning_rate=config.LEARNING_RATE,
-                    epochs=1,
+                    epochs=2,
                     layers='heads')
 
         # Training - Stage 2
